2ba863fbdc3891d8d9f09a0848291ff68ef9cd32
Butakov Vladimir <v_butakov@boxberry.ru>
2022-09-03 14:37:15 +0300

    Revert "HW5 Домашнее задание №5 «Параллельное исполнение»"
    This reverts commit 441e8b6611dd899047fbe574b6a3f4682e074d96.

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,65 +2,14 @@ package hw05parallelexecution
 
 import (
 	"errors"
-	"sync"
-	"sync/atomic"
 )
 
-var (
-	ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
-	ErrNoWorkers           = errors.New("no workers found")
-	ErrMaxErrorsIsNotValid = errors.New("max errors number is too small")
-)
+var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	waitGroup := sync.WaitGroup{}
-	tasksChanel := make(chan Task)
-	var errorsCount int64
-	var err error
-
-	if n <= 0 {
-		return ErrNoWorkers
-	}
-
-	if m <= 0 {
-		return ErrMaxErrorsIsNotValid
-	}
-
-	waitGroup.Add(n)
-
-	for i := 0; i < n; i++ {
-		go func() {
-			for v := range tasksChanel {
-				if v == nil {
-					continue
-				}
-
-				err := v()
-				if err != nil {
-					atomic.AddInt64(&errorsCount, 1)
-				}
-			}
-
-			waitGroup.Done()
-		}()
-	}
-
-	for _, task := range tasks {
-		if atomic.LoadInt64(&errorsCount) >= int64(m) {
-			err = ErrErrorsLimitExceeded
-
-			break
-		}
-
-		tasksChanel <- task
-	}
-
-	close(tasksChanel)
-
-	waitGroup.Wait()
-
-	return err
+	// Place your code here.
+	return nil
 }
