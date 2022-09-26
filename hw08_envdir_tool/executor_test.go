3792db638196b652655ce6b8b2547d89747a4e82
VladimirButakov <47273735+VladimirButakov@users.noreply.github.com>
2022-09-26 20:23:21 +0300

    Revert "hw08 Домашнее задание №8 «Утилита envdir»"
    This reverts commit 19016a2c67262fdc6fec17fd62a9e6f3da5e4bab.

--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,30 +1,7 @@
 package main
 
-import (
-	"runtime"
-	"testing"
-
-	"github.com/stretchr/testify/require"
-)
+import "testing"
 
 func TestRunCmd(t *testing.T) {
-	t.Run("empty cmd length", func(t *testing.T) {
-		strs := []string{}
-
-		code := RunCmd(strs, Environment{})
-
-		require.Equal(t, 1, code, "should return 1 code")
-	})
-
-	t.Run("test echo line return", func(t *testing.T) {
-		if runtime.GOOS == "windows" {
-			t.Skip()
-		}
-
-		strs := []string{"/bin/bash", "testdata/echoline.sh"}
-
-		code := RunCmd(strs, Environment{})
-
-		require.Equal(t, 0, code, "should return 0 code")
-	})
+	// Place your code here
 }
