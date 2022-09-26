33fa2d660909a1e561d0796eeb85cc37ffecf2bd
VladimirButakov <47273735+VladimirButakov@users.noreply.github.com>
2022-09-26 20:41:55 +0300

    hw08 Домашнее задание №8 «Утилита envdir»

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,39 @@
 package main
 
+import (
+	"log"
+	"os"
+	"os/exec"
+)
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+	if len(cmd) == 0 {
+		return 1
+	}
+
+	ex := exec.Command(cmd[0], cmd[1:]...) //nolint:gosec
+	envStrings := []string{}
+
+	for key, value := range env {
+		if value.NeedRemove {
+			os.Unsetenv(key)
+
+			continue
+		}
+
+		envStrings = append(envStrings,
+			key+"="+value.Value,
+		)
+	}
+
+	ex.Env = os.Environ()
+	ex.Env = append(ex.Env, envStrings...)
+
+	ex.Stdout = os.Stdout
+	if err := ex.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+	return ex.ProcessState.ExitCode()
 }
