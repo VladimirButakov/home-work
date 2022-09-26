19016a2c67262fdc6fec17fd62a9e6f3da5e4bab
VladimirButakov <47273735+VladimirButakov@users.noreply.github.com>
2022-09-26 20:22:00 +0300

    hw08 Домашнее задание №8 «Утилита envdir»

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,58 @@
 package main
 
-import "testing"
+import (
+	"io"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestReadDir(t *testing.T) {
 	// Place your code here
+	t.Run("check wrong folder", func(t *testing.T) {
+		_, err := ReadDir("testdata/nonexist")
+
+		require.Error(t, err, "wrong folder should return error")
+	})
+
+	t.Run("check empty folder", func(t *testing.T) {
+		dir, err := ioutil.TempDir("", "test")
+		require.Nilf(t, err, "err should be nil")
+
+		defer os.RemoveAll(dir)
+
+		envs, err := ReadDir(dir)
+
+		require.Equal(t, 0, len(envs), "envs length should be 0")
+		require.NoError(t, err, "should be done without errors")
+	})
+
+	t.Run("check folder", func(t *testing.T) {
+		dir, err := ioutil.TempDir("", "test")
+		require.Nilf(t, err, "err should be nil")
+
+		defer os.RemoveAll(dir)
+
+		file1, err := ioutil.TempFile(dir, "")
+		require.Nilf(t, err, "err should be nil")
+		io.WriteString(file1, "first")
+
+		fInfo1, err := file1.Stat()
+		require.Nilf(t, err, "err should be nil")
+
+		file2, err := ioutil.TempFile(dir, "")
+		require.Nilf(t, err, "err should be nil")
+		io.WriteString(file2, "second")
+
+		fInfo2, err := file2.Stat()
+		require.Nilf(t, err, "err should be nil")
+
+		envs, err := ReadDir(dir)
+
+		require.Equal(t, "first", envs[fInfo1.Name()].Value, "first test value should be equal to 'first'")
+		require.Equal(t, "second", envs[fInfo2.Name()].Value, "first test value should be equal to 'second'")
+		require.NoError(t, err, "should be done without errors")
+	})
 }
