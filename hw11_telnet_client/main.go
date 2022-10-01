0a7a82aa28bd41c8eb6fb813dce339b7aa974cb6
VladimirButakov <47273735+VladimirButakov@users.noreply.github.com>
2022-10-01 18:33:48 +0300

    hw11 Домашнее задание №11 «Клиент TELNET»

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,44 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"time"
+)
+
+var timeout time.Duration
+
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	host, port := os.Args[0], os.Args[1]
+	client := NewTelnetClient(net.JoinHostPort(host, port), timeout, os.Stdin, os.Stdout)
+	defer func(client TelnetClient) {
+		if err := client.Close(); err != nil {
+			return
+		}
+	}(client)
+
+	ctx, ctxCancelF := context.WithCancel(context.Background())
+
+	go func() {
+		defer ctxCancelF()
+
+		err := client.Send()
+		if err != nil {
+			if _, err := fmt.Fprintln(os.Stderr, err); err != nil {
+				return
+			}
+		}
+	}()
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt)
+
+	select {
+	case <-sigCh:
+	case <-ctx.Done():
+		close(sigCh)
+	}
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"net"
+	"os"
 	"time"
 )
 
@@ -12,10 +15,57 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type Client struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &Client{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+func (c *Client) Close() (err error) {
+	err = c.conn.Close()
+	return
+}
+
+func (c *Client) Send() error {
+	if _, err := io.Copy(c.conn, c.in); err != nil {
+		return fmt.Errorf("error occurred while sending: %w", err)
+	}
+
+	if _, err := fmt.Fprintln(os.Stderr, "...EOF"); err != nil {
+		return err
+	}
+
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (c *Client) Receive() error {
+	if _, err := io.Copy(c.out, c.conn); err == nil {
+		return fmt.Errorf("error occurred while receiving: %w", err)
+	}
+	if _, err := fmt.Fprintln(os.Stderr, "...Connection was closed by peer"); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *Client) Connect() error {
+	conn, err := net.DialTimeout("tcp", c.address, c.timeout)
+	if err != nil {
+		return fmt.Errorf("connection error: %w", err)
+	}
+	c.conn = conn
+
+	return nil
+}
