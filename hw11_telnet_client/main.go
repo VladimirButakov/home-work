97bdb85633f07c8f3953e90f09ba3af2f1113e39
VladimirButakov <47273735+VladimirButakov@users.noreply.github.com>
2022-10-02 12:32:17 +0300

    hw11 правки

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"log"
 	"net"
@@ -14,46 +13,50 @@ import (
 var timeout time.Duration
 
 func main() {
-	if flag.NArg() < 2 {
-		log.Fatal("not enough arguments")
-	}
+	host, port := os.Args[0], os.Args[1]
+	client := NewTelnetClient(net.JoinHostPort(host, port), timeout, os.Stdin, os.Stdout)
 
-	host := flag.Arg(0)
-	port := flag.Arg(1)
-	address := net.JoinHostPort(host, port)
-
-	telnet := NewTelnetClient(address, timeout, os.Stdin, os.Stdout)
-
-	if err := telnet.Connect(); err != nil {
+	if err := client.Connect(); err != nil {
 		log.Fatal(err)
 	}
-	defer telnet.Close()
 
 	ctx, ctxCancelF := context.WithCancel(context.Background())
-	go func() {
-		defer ctxCancelF()
-
-		err := telnet.Send()
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-	}()
-
-	go func() {
-		defer ctxCancelF()
-
-		err := telnet.Receive()
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-	}()
 
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, os.Interrupt)
+
+	defer func(client TelnetClient) {
+		if err := client.Close(); err != nil {
+			return
+		}
+	}(client)
+
+	send(client, ctxCancelF)
+	receive(client, ctxCancelF)
 
 	select {
 	case <-sigCh:
 	case <-ctx.Done():
 		close(sigCh)
 	}
+}
+
+func send(client TelnetClient, ctxCancelF context.CancelFunc) {
+	err := client.Send()
+	if err != nil {
+		if _, err := fmt.Fprintln(os.Stderr, err); err != nil {
+			return
+		}
+	}
+	ctxCancelF()
+}
+
+func receive(client TelnetClient, ctxCancelF context.CancelFunc) {
+	err := client.Receive()
+	if err != nil {
+		if _, err := fmt.Fprintln(os.Stderr, err); err != nil {
+			return
+		}
+	}
+	ctxCancelF()
 }
